10e344c75efc675f0ece4c5f4d7b0d3516cd8ad1
HazelYe2 <Hazel.Ye25@ycdsbk12.ca>
2024-03-28 14:18:40 -0400

    D1GramsToPounds.java

--- D1GramsToPounds.java
+++ D1GramsToPounds.java
@@ -1,11 +1,19 @@
 class D1GramsToPounds extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * Write a program that outputs a formatted table that shows a conversion from grams to pounds from 100 to 1000 grams.
+  * The table should display for every 100 grams.
+  * The pounds values must be calculated.
+  * The grams column should be formatted to take up 6 spaces
+  * The pounds column should be formatted to take up 10 spaces.
+  * @author: H. Ye
   */
   
   public void run() {
+    //output table header
+    System.out.printf("%5s, %10s, %n", "grams", "pounds");
+    System.out.println ("---------------");
+
     
   }
 }
