df060cb7cea44ca27ca21ab6d74024ecd88fd526
HazelYe2 <Hazel.Ye25@ycdsbk12.ca>
2024-03-30 17:35:26 -0400

    B1DiceGame.java

--- B1DiceGame.java
+++ B1DiceGame.java
@@ -1,12 +1,26 @@
 class B1DiceGame extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * Write a program that lets you simulate the rolling of 100 pairs of dice. For each roll, 
+  * if the pair adds up to 2, print "snake eyes!"
+  * If the pair adds up to 7, print "lucky seven"
+  * if the pair adds up to any other sum, do not print out anything
+  * @author: H. Ye
   */
   
   public void run() {
-    
+    int rolls = 100;
+    int dieOne;
+    int dieTwo;
+    int total = dieOne + dieTwo;
+
+    if (total == 2) {
+      System.out.println ("Snake eyes!");
+    }
+
+    if (total == 7) {
+      System.out.println ("Lucky seven!");
+    }
   }
 }
 
