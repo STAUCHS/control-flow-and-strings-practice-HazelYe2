fd0d849c08ca599e364ca548202d75a411a14736
HazelYe2 <Hazel.Ye25@ycdsbk12.ca>
2024-04-02 12:47:10 -0400

    C2EveryNth.java

--- C2EveryNth.java
+++ C2EveryNth.java
@@ -1,11 +1,25 @@
 class C2EveryNth extends ConsoleProgram {
 
   /**
-  * Description
+  * Given a non-empty string and an int N, return the string made starting with character 0, and then every Nth char of the string
+  * So if N is 3, use char 0, 3, 6, ... and so on. N is 1 or more
   * @author:
   */
   
   public void run() {
-    
+
+    // Declare variables and get user input
+    String word = readLine ("Input a word: ");
+    int integer = readInt ("Enter an integer: ");
+    int length;
+    int newWord;
+    int nthChar;
+
+    length = word.length();
+    newWord = "";
+
+    for (int counter = 0; counter <= length; counter += nthChar)
+
+
   }
 }
