5ada92e137e215e81ee62e3263a0977934924a2c
HazelYe2 <Hazel.Ye25@ycdsbk12.ca>
2024-04-02 11:23:53 -0400

    A2HasTeen.java

--- A2HasTeen.java
+++ A2HasTeen.java
@@ -1,11 +1,23 @@
 class A2HasTeen extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * We'll say that a number is "teen" if it is in the range 13..19 inclusive. 
+  * Given 3 int values via user input, output true if 1 or more of them are teen.
+  * @author: H. Ye
   */
   
   public void run() {
+    int firstAge = readInt ("Input the first value: ");
+    int secondAge = readInt ("Input the second value: ");
+    int thirdAge = readInt ("Input the third value: ");
     
+    if (firstAge >= 13 && firstAge <= 19 || secondAge >= 13 && secondAge <= 19 || thirdAge >=13 && secondAge <=19)
+    {
+      System.out.println ("True");
+    }
+    else
+    {
+      System.out.println ("False");
+    }
   }
 }
