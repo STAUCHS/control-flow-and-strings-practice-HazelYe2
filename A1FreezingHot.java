083b9f067ae56ab41850b7b0f4cee76bb8838dc9
HazelYe2 <Hazel.Ye25@ycdsbk12.ca>
2024-04-02 11:23:46 -0400

    A1FreezingHot.java

--- A1FreezingHot.java
+++ A1FreezingHot.java
@@ -1,11 +1,26 @@
 class A1FreezingHot extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * Given two temperatures via user input, output true if one is less than 0 and the other is greater than 100
+  * @author: H. Ye
   */
   
   public void run() {
-    
+
+    // Get user input
+
+    int tempOne = readInt ("Input the first temperature: ");
+    int tempTwo = readInt ("Input the second temperature: ");
+
+    // Output true if one is less than 0 and the other is greater than 100
+    if (tempOne < 0 && tempTwo > 100 || tempOne > 100 && tempTwo < 0)
+    {
+      System.out.println ("True");
+    }
+    else 
+    {
+      System.out.println ("False");
+    }
+
   }
 }
