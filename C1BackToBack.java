ada1efab09f52244db3d3ddfd984b4b288fa1498
HazelYe2 <Hazel.Ye25@ycdsbk12.ca>
2024-04-02 12:40:45 -0400

    C1BackToBack.java

--- C1BackToBack.java
+++ C1BackToBack.java
@@ -1,11 +1,23 @@
 class C1BackToBack extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * Given a string, take the last character and print a new string with the last character added at the front and back, so "cat" yields "tcatt". 
+  * The original string will be length 1 or more.
+  * @author: H. Ye
   */
   
   public void run() {
-    
+
+    // Declare variables
+    String word = readLine ("Input any word: ");
+    String lastChar;
+    String newWord;
+
+    // Add last character to front and back
+    lastChar = word.substring(word.length() - 1);
+    newWord = lastChar + word + lastChar;
+
+    // Ouput new word
+    System.out.println (newWord);
   }
 }
