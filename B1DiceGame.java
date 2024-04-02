cca193505861b2803e8459f49ca2addef61ac7dc
HazelYe2 <Hazel.Ye25@ycdsbk12.ca>
2024-04-02 11:31:30 -0400

    B1DiceGame.java

--- B1DiceGame.java
+++ B1DiceGame.java
@@ -10,13 +10,18 @@ class B1DiceGame extends ConsoleProgram {
   
   public void run() {
     int rolls = 100;
-    Random rand = new Random();
+    Random roll = new Random();
 
-
-    for (int i = 0; i < rolls; i++);
+    // Declare variables
     int dieOne;
     int dieTwo;
     int total = dieOne + dieTwo;
+
+    for (int counter = 0; counter < rolls; counter++);
+    {
+      dieOne = roll.nextInt (1, 6);
+      dieTwo = roll.nextInt (1, 6);
+    }
 
     if (total == 2) {
       System.out.println ("Snake eyes!");
