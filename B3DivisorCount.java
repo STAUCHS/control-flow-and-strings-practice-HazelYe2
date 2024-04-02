0dfeec9e85749f42ba5912fb5735665668b7e368
HazelYe2 <Hazel.Ye25@ycdsbk12.ca>
2024-04-02 12:40:55 -0400

    B3DivisorCount.java

--- B3DivisorCount.java
+++ B3DivisorCount.java
@@ -1,12 +1,24 @@
 class B3DivisorCount extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * Enter an integer and that prints all the divisors of that number
+  * Note that d is a divisor of n if n%d=0
+  * @author: H. Ye
   */
   
   public void run() {
-    
+
+    // Declare variable and get user input
+    int integer = readInt ("Enter an integer: ");
+
+    // Check for divisors between 1 and integer
+    for (int divisor = 1; divisor <= integer; divisor++)
+    {
+      if (integer % divisor == 0) 
+      {
+        System.out.println (divisor);
+      }
+    }
   }
 }
 
