ed83bcee297ca9262e27dd6ac1551692fe33c100
HazelYe2 <Hazel.Ye25@ycdsbk12.ca>
2024-04-02 11:31:22 -0400

    A3Within10.java

--- A3Within10.java
+++ A3Within10.java
@@ -1,11 +1,16 @@
 class A3Within10 extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * Given 2 int values via user input, output whichever value is nearest to the value 10, or output 0 in the event of a tie
+  * Note that Math.abs(n) returns the absolute value of a number
+  * @author: H. Ye
   */
   
   public void run() {
+    int valueOne = readInt ("Input the first value: ");
+    int valueTwo = readInt ("Input the second value: ");
+
+    if ()
     
   }
 }
