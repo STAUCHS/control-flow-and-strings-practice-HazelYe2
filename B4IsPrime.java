662bfbfd52127101b14ac1cc8d2d170c93fabc09
HazelYe2 <Hazel.Ye25@ycdsbk12.ca>
2024-04-02 12:41:02 -0400

    B4IsPrime.java

--- B4IsPrime.java
+++ B4IsPrime.java
@@ -1,11 +1,15 @@
 class B4IsPrime extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * Enter an integer, and that will print appropriately "is a prime number" or "is a composite number"
+  * A number n is a prime number, if it has no divisors d, where or more conveniently, where 
+  * @author: H. Ye
   */
   
   public void run() {
+    
+    // Declare variable and get user input
+    int integer = readInt ("Input an integer: ");
     
   }
 }
